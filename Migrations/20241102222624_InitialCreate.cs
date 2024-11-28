36e7d8416d50294d686fbd5fffb4fd2d3b93fd39
sharman28 <sharman28@coventry.ac.uk>
2024-11-28 17:38:14 +0000

    20241102222624_InitialCreate.cs

--- Migrations/20241102222624_InitialCreate.cs
+++ Migrations/20241102222624_InitialCreate.cs
@@ -5,10 +5,10 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace JKLHealthcare.Migrations
 {
-    /// <inheritdoc />
+   
     public partial class InitialCreate : Migration
     {
-        /// <inheritdoc />
+       
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
@@ -29,7 +29,7 @@ namespace JKLHealthcare.Migrations
                 });
         }
 
-        /// <inheritdoc />
+        
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
